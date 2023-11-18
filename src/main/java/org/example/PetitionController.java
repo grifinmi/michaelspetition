c83f56a8c0b379442dc0a74c745cc3c607083ad8
grifnmi <mgriffin37@universityofgalway.ie>
2023-11-18 17:34:27 +0000

    18/11 - can save and display a petition

--- src/main/java/org/example/PetitionController.java
+++ src/main/java/org/example/PetitionController.java
@@ -1,6 +1,7 @@
 package org.example;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,20 +26,24 @@ public class PetitionController {
     /* This code is executed when the user presses the submit button on the enterPetition page*/
     public String processForm
             (@RequestParam("petitionTitle") String pName,
-             @RequestParam("petitionDetail") String pDetail) {
+             @RequestParam("petitionDetail") String pDetail,
+            Model model) {
 
         Petition p = new Petition();
         p.setPetitionDetail(pDetail);
         p.setPetitionTitle(pName);
         pList.add(p);
-        for (Petition pl : pList) {
-            System.out.println (pl.getPetitionDetail());
-        }
+        model.addAttribute("Petitions",pList);
+
         return "DisplayPetitions";
     }
     @GetMapping("/DisplayPetitions")
     public String displayForm() {
 
+        for (Petition pl : pList) {
+            System.out.print("In @GetMapping(/DisplayPetitions)");
+            System.out.println (pl.getPetitionDetail());
+        }
         return "DisplayPetitions";
 }
 
