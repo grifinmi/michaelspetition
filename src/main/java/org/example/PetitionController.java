67107c987062d1c8e3b8fe9de0f54acdbdc703e5
grifnmi <mgriffin37@universityofgalway.ie>
2023-11-23 10:28:11 +0000

    23/11 - enter working, display working , search screen brings back to display petitions

--- src/main/java/org/example/PetitionController.java
+++ src/main/java/org/example/PetitionController.java
@@ -10,15 +10,20 @@ import java.util.ArrayList;
 
 @Controller
 public class PetitionController {
-    private ArrayList <Petition> pList = new ArrayList<Petition>();
-
+    private ArrayList<Petition> pList = new ArrayList<Petition>();
 
     @GetMapping("/")
     public String home() {
+
+        /*This code is called on launch of localhost:8080/ */
+        System.out.println("in GetMapping/");
         return "EnterPetition";
     }
+
     @GetMapping("/EnterPetition")
     public String enterPetition() {
+        /*This code is called on launch of localhost:8080/EnterPetition */
+        System.out.println("GetMapping/enterpetition");
         return "EnterPetition";
     }
 
@@ -27,25 +32,43 @@ public class PetitionController {
     public String processForm
             (@RequestParam("petitionTitle") String pName,
              @RequestParam("petitionDetail") String pDetail,
-            Model model) {
-
+             Model model) {
+        /*This code is called when submit button is pressed to enter petition */
+        System.out.println("@PostMapping(/EnterPetition)");
         Petition p = new Petition();
         p.setPetitionDetail(pDetail);
         p.setPetitionTitle(pName);
         pList.add(p);
-        model.addAttribute("Petitions",pList);
+        model.addAttribute("Petitions", pList);
 
         return "DisplayPetitions";
     }
-    @GetMapping("/DisplayPetitions")
-    public String displayForm() {
 
-        for (Petition pl : pList) {
-            System.out.print("In @GetMapping(/DisplayPetitions)");
-            System.out.println (pl.getPetitionDetail());
+    @GetMapping("/showSearchPetitionPage")
+    public String showSearchPetitionPage(String search) {
+        System.out.println("@GetMapping(\"/showSearchPetitionPage\")    ");
+        return "Searchpetition";
+    }
+
+    @PostMapping("/SearchPetition")
+    public String searchPetition(@RequestParam (name = "petitionSearch") String search) {
+
+        System.out.println("in postMapping/searchpetition");
+        Petition petition = new Petition();
+
+        for (Petition p : pList) {
+            System.out.print("in for and string is >>>");
+            System.out.println(search);
+            if (p.getPetitionDetail().contains(search)) {
+                petition = p;
+                break;
+            } else if (p.getPetitionTitle().contains(search)) {
+                petition = p;
+                break;
+            }
         }
+        System.out.println("out of  for");
+        System.out.println(petition.getPetitionDetail());
         return "DisplayPetitions";
-}
-
-
+    }
 }
